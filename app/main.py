af84721ddef81c53b6eb45aef7e64223d988339b
Samira Afrin Alam Shopnil <samiraafrin.alam@gmail.com>
2025-02-01 02:29:14 +0200

    Add requirements.txt, update .gitignore, remove debug comment

--- app/main.py
+++ app/main.py
@@ -7,7 +7,5 @@ app.include_router(seashell_router)
 
 @app.get("/")
 def status():
-    # data = "Hello world!"
-    # return Response(content=data, media_type="text/plain")
 
     return "Program is running"
